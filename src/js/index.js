32785a270cbd8c2f36a2e7efcd309c692c5d3173
Piotr Lewczyk <lewczykpiotr@gmail.com>
2022-09-17 17:29:59 +0200

    feat: fix displaying github icon, links in ne tab

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 import '../scss/main.scss';
 
+console.log("Hi, I'm Piotr - nice to meet you!");
+
 fetch('https://api.github.com/users/lewpiotr1701/repos?sort=created&direction=desc')
   .then(response => response.json())
   .then(response => {
@@ -7,17 +9,23 @@ fetch('https://api.github.com/users/lewpiotr1701/repos?sort=created&direction=de
     for (let repository of response) {
         if (repository["homepage"]) {
             const {name, html_url, description, homepage} = repository;
-            const myTemplate = `<article class="project">
+
+            let newName = name;
+            if (name == 'lewpiotr1701.github.io') {
+                newName = 'homepage';
+            }
+
+            const template = `<article class="project">
       <div class="project__upper-frame">
         <span class="project__upper-frame--circle"></span>
         <span class="project__upper-frame--circle"></span>
         <span class="project__upper-frame--circle"></span>
       </div>
       <div class="project__content-frame">
-        <img src="../assets/img/github-icon-dark.svg" alt="">
+        <img src="img/github-icon-dark.svg" alt="">
         <h3 class="project__grid project__title">
           <span class="project__label">project:</span>
-          <span>${name}</span>
+          <span>${newName}</span>
         </h3>
         <p class="project__grid project__grid--description">
           <span class="project__label">description:</span>
@@ -26,18 +34,22 @@ fetch('https://api.github.com/users/lewpiotr1701/repos?sort=created&direction=de
         <p class="project__grid">
           <span class="project__label">demo:</span>
           <span>
-            &lt;<a class="project__link" href="${homepage}" title="${name} - demo">see here</a>&gt;
+            &lt;<a class="project__link" href="${homepage}" title="${newName} - demo" target="_blank" rel="noopener noreferrer">
+                    see here
+                </a>&gt;
           </span>
         </p>
         <p class="project__grid">
           <span class="project__label">github:</span>
           <span>
-            &lt;<a class="project__link" href="${html_url}" title="${name} - source code">source code</a>&gt;
+            &lt;<a class="project__link" href="${html_url}" title="${newName} - source code" target="_blank" rel="noopener noreferrer">
+                    source code
+                </a>&gt;
           </span>
         </p>
       </div>
       </article>`;
-      projectsGrid.innerHTML += myTemplate;
+      projectsGrid.innerHTML += template;
     }
         }
 })
